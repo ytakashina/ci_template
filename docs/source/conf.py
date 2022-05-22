cebed9f4cc0c3f951d09ebc10af0c7a7f5b5db85
Yuya Takashina <takashina2051@gmail.com>
2022-05-23 08:09:06 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 
-project = "opt_app_boilerplate"
+project = "ci_template"
 copyright = "2022, Yuya Takashina"
 author = "Yuya Takashina"
 
